68987612cde733cd58bb2ef6326e684126a81cae
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2022-01-15 13:13:36 +0800

    test for building erc1155 merkle tree added

--- packages/drop-sdk/test/main.test.ts
+++ packages/drop-sdk/test/main.test.ts
@@ -1,8 +1,9 @@
 import { buildMerkleTreeERC1155 } from '../src'
+import { hexlify } from '@ethersproject/bytes';
 // import { randomBytes } from '@ethersproject/random';
 // import { computeAddress } from '@ethersproject/transactions';
 // import { SigningKey } from '@ethersproject/signing-key';
-import { ethers } from 'ethers'
+// import { ethers } from 'ethers'
 
 type DataOldFormat = {
     [account: string]: {
@@ -22,32 +23,21 @@ type DataOldFormat = {
 
 describe('buildMerkleTreeERC1155', () => {
     it('should build Merkle tree for erc1155 NFTs', async () => {
-        console.log('gn')
-        const wallet1 = ethers.Wallet.createRandom()
-        const wallet2 = ethers.Wallet.createRandom()
-        const wallet3 = ethers.Wallet.createRandom()
-
-        const data: DataOldFormat = {}
-
-        data[wallet1.address] = {
-            amount: 1,
-            tokenId: 1,
-            maxSupply: 3
+        const data: DataOldFormat = {
+            '0x88EC069E5151cFad602A06AD13fea7e87Ba089C8': { amount: 1, tokenId: 1, maxSupply: 3 },
+            '0x092464dFF81eAb2111D0A7f8d6334Ef31f4eF5Da': { amount: 1, tokenId: 1, maxSupply: 3 },
+            '0xBb41E77278c0f4afE9F4cF1Ee8866432FE024482': { amount: 1, tokenId: 1, maxSupply: 3 }
         }
-
-        data[wallet2.address] = {
-            amount: 1,
-            tokenId: 1,
-            maxSupply: 3
-        }
-        data[wallet3.address] = {
-            amount: 1,
-            tokenId: 1,
-            maxSupply: 3
-        }
-
 
         const result = buildMerkleTreeERC1155(data);
-        console.log({ result })
+
+        expect(Object.keys(result.claims).length).toEqual(3);
+        expect(result.merkleRoot).toEqual('0xf01f308c9098b219e95cde85b9a69256ab5d5aad63728e9a8036044c3f95937c');
+
+        const claim = result.claims['0x88EC069E5151cFad602A06AD13fea7e87Ba089C8'];
+        expect(claim.amount).toEqual(hexlify(1));
+        expect(claim.tokenId).toEqual(1);
+        expect(claim.maxSupply).toEqual(hexlify(3));
+        expect(claim.proof.length).toBeGreaterThan(0);
     })
 })
