9baa386d927b9b5c9468158de618de39c767f340
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2022-01-20 20:01:48 +0800

    scripts added

--- packages/drop-contracts/hardhat.config.ts
+++ packages/drop-contracts/hardhat.config.ts
@@ -10,21 +10,21 @@ import "solidity-coverage";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.10",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    solidity: "0.8.10",
+    networks: {
+        rinkeby: {
+            url: process.env.RINKEBY_URL || "",
+            accounts:
+            process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
