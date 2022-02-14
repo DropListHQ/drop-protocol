4cd3984f919e72ef8344c4da095d3a2b248fb27d
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2022-02-14 13:59:43 +0800

    chainId, tokenAddress, type, claims are added to main Drop object

--- packages/drop-sdk/src/Drop.ts
+++ packages/drop-sdk/src/Drop.ts
@@ -1,12 +1,19 @@
-import { TDropMetadata } from './types';
+import { TDropMetadata, TRecipientsData } from './types';
 
 export default class Drop {
-
     public readonly ipfshash: string;
     public readonly metadata: TDropMetadata;
+    public readonly chainId: number;
+    public readonly tokenAddress: string;
+    public readonly type: string;
+    public readonly claims: TRecipientsData;
 
     constructor(ipfshash: string, metadata: TDropMetadata) {
         this.ipfshash = ipfshash;
         this.metadata = metadata;
+        this.chainId = metadata.chainId;
+        this.tokenAddress = metadata.tokenAddress;
+        this.type = metadata.type;
+        this.claims = metadata.claims;
     }
 }
--- packages/drop-sdk/test/drop-sdk.test.ts
+++ packages/drop-sdk/test/drop-sdk.test.ts
@@ -9,5 +9,14 @@ describe('#getDrop', () => {
 
         const drop = await dropSDK.getDrop(ipfshash);
         expect(drop.ipfshash).toEqual(ipfshash);
+
+        expect(drop.chainId).toEqual(4);
+        expect(drop.tokenAddress).toEqual("0x568b228eb07cd317ac5fbeff620379145380a823");
+        expect(drop.type).toEqual("erc20");
+        expect(Object.keys(drop.claims).length).toEqual(2);
+
+        // metadata
+        expect(drop.metadata.description).toEqual("Test ERC20 (#1)");
+        expect(drop.metadata.title).toEqual("Test ERC20 (#1)");
     })
 })
